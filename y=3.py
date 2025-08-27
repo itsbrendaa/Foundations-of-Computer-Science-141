c6e2714028fad2f028e0b23aa17ab48fdc1aeada
Brenda Kouam <brenda.kouam001@albright.edu>
2025-08-27 09:30:50 -0400

    Add files via upload

---
+++ y=3.py
@@ -0,0 +1,16 @@
+y=3
+x=7+y
+print (x)
+greeting= "4545"
+print (greeting)
+#what does in parameters are "strings" "integer" "float" or "boolean (ex: true or false)"
+print (greeting.center(24,'>'))
+print (greeting.isdecimal())
+temperature = 85
+if not (temperature != 100):
+    print("its hot outside")
+    print ("i need some water")
+print ("its a great day")
+# "=" means assigment
+# "==" mmeans equality/ "are these  things the same"
+# "!=" means notequal
