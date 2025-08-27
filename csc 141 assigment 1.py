be8da6fe30b532666ec7a9076bf832aed5f9ca58
Brenda Kouam <brenda.kouam001@albright.edu>
2025-08-27 19:58:40 -0400

    Add files via upload

---
+++ csc 141 assigment 1.py
@@ -0,0 +1,5 @@
+print ("Hello Python interpreter!")
+1-2 print ("hello"())
+Print ('hello')
+#the typo i made was adding parenteces in the parameters of the print function. this reslted in a "invalid syntax" mesage becuase the arrangment of the code in being disrupted. The 2nd typo i made was i use 1 quotation instead of 2 but that did not affect the code because we learned today both quatations mark do the same thing
+1-3 #If I had infinite programming skills, I’d want to create a social media platform that’s both educational and enjoyable—something that actually helps people learn and grow while having fun. The goal would be to slow down the “brain rot” that often comes with endless scrolling and shallow content.
