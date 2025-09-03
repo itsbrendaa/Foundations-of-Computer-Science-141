d4d36fea2a5cbaa141bb767b561ee12765a9c203
itsbrendaa <brenda.kouam001@albright.edu>
2025-09-03 09:24:22 -0400

    assignment2

---
+++ assignment 2/assignment2.py
@@ -0,0 +1,57 @@
+#aassigment 2
+print("2-1")
+message = "hi Brenda"
+
+print (message)
+print("2-2")
+message = "how are you"
+print(message)
+
+#working with names
+print("2-3")
+first_name= "Brenda"
+last_name="kouam"
+full_name=f"Brenda Kouam"
+message= "how was your day today?"
+print(full_name, message)
+
+print("2-4")
+p= full_name
+print(p.upper())
+print(p.lower())
+print(p.title)
+
+print("2-5")
+message= 'Dr.Martin Luther King once said "i have a dream..."'
+print(message)
+#long live king
+
+print("2-6")
+famous_person= "Muhammad Ali"
+message= 'once said "i foat like a butterfly, i sting like a bee..."'
+print (famous_person, message)
+
+print("2-7")
+b=" brenda "
+print(b.lstrip())
+print(b.rstrip())
+print(b.strip())
+
+print("2-8")
+filename= "python_notes.txt"
+print(filename.removesuffix(".txt"))
+
+print("2-9")
+print(4+4)
+print(8-0)
+print(8*1)
+print(16-2)
+#this was simple but its funny how i kept acedentaly doing this when i was writting down the quetion i was on
+
+print("2-10")
+favorite_number=7
+message= "my favorite number is..."
+print (message, favorite_number)
+print ("2-11")
+message="i added a comment to 2-3, 2-5, and 2-9"
+print (message)
