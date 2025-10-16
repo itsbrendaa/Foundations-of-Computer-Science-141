1c6a27c9c811825c648bcae77e6c7409e7c49dfa
itsbrendaa <brendaskouam@gmail.com>
2025-10-15 22:57:44 -0400

    done

--- assignment 2/assigment7.py
+++ assignment 2/assigment7.py
@@ -26,18 +26,19 @@ else:
 print("7-4")
 
 toppings = []
-topping = ""
 
-while topping != "im done":
-    topping = input('Enter a topping or type "im done" to finish:')
+while True:
+    topping = input('Enter a topping or type "im done" to finish: ')
+    if topping.lower() == "im done":
+        break
     toppings.append(topping)
 
-print(f"your pizza toppings are: {toppings}")
+print("7-5")
 
-("7-5")
 while True:
-    age = input('Please enter your age or type "quit" to exit:')
-
+    age = input('Please enter your age or type "quit" to exit: ')
+    if age.lower() == "quit":
+        break
     age = int(age)
     if age < 3:
         print("Your ticket is free!")
@@ -45,5 +46,67 @@ while True:
         print("Your ticket costs $10.")
     else:
         print("Your ticket costs $15.")
-    if age() == 'quit':
+
+print("7-6")
+
+age = ''
+while age != 'quit':
+    age = input("Enter your age or 'quit' to stop: ")
+    if age == 'quit':
         break
+    age = int(age)
+    if age < 3:
+        print("Ticket is free.")
+    elif 3 <= age <= 12:
+        print("Ticket costs $10.")
+    else:
+        print("Ticket costs $15.")
+
+print("7-7")
+
+#while True:
+    #print("This loop won't end")
+
+print("7-8")
+
+sandwich_orders = ['tuna', 'ham', 'turkey', 'chicken', 'veggie']
+finished_sandwiches = []
+
+while sandwich_orders:
+    sandwich = sandwich_orders.pop(0)
+    print(f"I made your {sandwich} sandwich.")
+    finished_sandwiches.append(sandwich)
+
+print("7-9")
+
+sandwich_orders = ['tuna', 'pastrami', 'ham', 'pastrami', 'turkey', 'pastrami', 'chicken']
+finished_sandwiches = []
+
+print("Sorry, the deli has run out of pastrami!\n")
+
+while 'pastrami' in sandwich_orders:
+    sandwich_orders.remove('pastrami')
+
+while sandwich_orders:
+    sandwich = sandwich_orders.pop(0)
+    print(f"I made your {sandwich} sandwich.")
+    finished_sandwiches.append(sandwich)
+
+print("7-10")
+responses = {}
+
+polling_active = True
+
+while polling_active:
+    name = input("What's your name? ")
+    vacation = input("If you could visit one place in the world, where would you go? ")
+
+    responses[name] = vacation
+
+    repeat = input("Would you like to let another person respond? (yes/no) ")
+    if repeat == 'no':
+        polling_active = False
+
+print("Dream Vacation Poll Results")
+for name, place in responses.items():
+    print(f"{name.title()} would like to visit {place.title()}.")
